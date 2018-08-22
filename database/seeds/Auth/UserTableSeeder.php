0621cd236da34cf4a5e756ae4003609736e0be29
jorgimello <joorgemelo@gmail.com>
2018-08-21 22:08:03 -0300

    first commit

---
+++ database/seeds/Auth/UserTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Auth\User;
+use Illuminate\Database\Seeder;
+
+/**
+ * Class UserTableSeeder.
+ */
+class UserTableSeeder extends Seeder
+{
+    use DisableForeignKeys;
+
+    /**
+     * Run the database seed.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->disableForeignKeys();
+
+        // Add the master administrator, user id of 1
+        User::create([
+            'first_name'        => 'Admin',
+            'last_name'         => 'Istrator',
+            'email'             => 'joorgemelo@gmail.com',
+            'password'          => 'jorge190493',
+            'confirmation_code' => md5(uniqid(mt_rand(), true)),
+            'confirmed'         => true,
+        ]);
+
+        $this->enableForeignKeys();
+    }
+}
